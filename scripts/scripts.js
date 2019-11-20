99850b2cbef0ca067bca17d09e0945783667b99d
Rang3rDang3r <stu-evans@live.co.uk>
2019-11-20 18:10:37 +0000

    repeated code for each apart from the final element

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,20 @@
 
 whatCanIDrink = function(age){
 
+/*Set parameters */
     if (age <= 0) {
-    return "Sorry. I can’t tell what drink because that age is incorrect!";
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
     }   
     else if ((age >0) && (age <=13)) {
-    return "Drink Toddy";
+        return "Drink Toddy";
     }
-
+    else if ((age >13) && (age <=17)) {
+        return "Drink Coke";
+    }
+    else if ((age >17) && (age <=21)) {
+        return "Drink Beer";
+    }
+    else if ((age > 21) && (age <=130)) {
+        return "Drink Whiskey";
+    }
 };
--- scripts/testSpec.js
+++ scripts/testSpec.js
@@ -2,7 +2,7 @@ describe("My whatCanIDrink function", function(){
 
     /* What it does */
     describe("checks age", function(){
-        it("should exist", function(){
+        it("should return a result of - Sorry. I can’t tell what drink because that age is incorrect!", function(){
 
         /* Checks to see if action being performed is correct */
             expect(whatCanIDrink) .toBeDefined();
@@ -11,6 +11,18 @@ describe("My whatCanIDrink function", function(){
         it("should return result Drink Toddy called as whatCanIDrink(13)", function(){
             var result= whatCanIDrink(13);
             expect(result) .toBe("Drink Toddy");
+        })
+        it("should return result Drink Coke called as whatCanIDrink(17)", function(){
+            var result= whatCanIDrink(17);
+            expect(result) .toBe("Drink Coke");
+        })
+        it("should return result Drink Beer called as whatCanIDrink(20)", function(){
+            var result= whatCanIDrink(20);
+            expect(result) .toBe("Drink Beer");
+        })
+        it("should return result Drink Whiskey called as whatCanIDrink(129)", function(){
+            var result= whatCanIDrink(129);
+            expect(result) .toBe("Drink Whiskey");
         })
     })
 })
