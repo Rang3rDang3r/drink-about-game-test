f18a8cccca8053cc10beadb0a658280952bbbbd3
Rang3rDang3r <stu-evans@live.co.uk>
2019-11-20 13:13:31 +0000

    added basic test for age to be whiskey

---
+++ scripts/testSpec.js
@@ -0,0 +1,7 @@
+describe ("whatCanIDrink", function() {
+    describe("age function", function(){
+        it("will return whiskey", function(){
+            expect(age()) .toBe("Whiskey");
+        })
+    })
+})
